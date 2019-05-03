ad65a206923adf8ee9e280e91da5da66d85c044b
Levon Isajanyan <levonisadjanyan@gmail.com>
2019-05-03 11:10:50 +0400

    Add core files

---
+++ js/jsviewport.js
@@ -0,0 +1,68 @@
+//Height of blocks :
+$( document ).ready(function() {
+    var viewportHeight = $(window).height();
+    var header = $('header').outerHeight(true);
+    var about = viewportHeight-header;
+    $('#aboutme').css({
+        "height": about
+    });
+    $('#aboutme img').css({
+        "height": about
+    });
+    $('#footer').css('height',about);
+});
+
+
+
+//type animation
+$(document).ready(function () {
+    $(function(){
+        $("#typed").typed({
+            stringsElement: $('#bio'),
+            showCursor: false
+        });
+    });
+});
+
+//brain increase percentage
+$(document).ready(function () {
+    $('#count').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 66000,
+            easing: 'linear',
+            step: function (now) {
+                $(this).text(Math.ceil(now)).append(' %');
+            }
+        });
+    });
+});
+
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        //Navigation
+        menu: '#menu',
+        navigation: true,
+        navigationPosition: 'right',
+
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 1000,
+        autoScrolling:true,
+        fitToSection: false,
+        //disable when mobile will be ready
+        scrollBar: true,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+
+        //Accessibility
+        keyboardScrolling: true,
+        recordHistory: true,
+
+        //Design
+        verticalCentered: true
+    });
+});
+
+
